8bd984f360fee1ae0d69fc3196d2ef22cafe4abb
Yisedcasti <yisedcastiblanco@gmail.com>
2025-02-11 22:47:36 -0500

    Update inicio2.php

--- inicio2.php
+++ inicio2.php
@@ -42,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../edufast/css/inicio2.css">
+    <link rel="stylesheet" href="css/inicio2.css">
     <title>Login</title>
 </head>
 

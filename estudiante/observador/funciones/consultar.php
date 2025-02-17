4373b1489a499d77fec484faa4652528cbd59539
Yisedcasti <yisedcastiblanco@gmail.com>
2025-02-16 23:24:34 -0500

    Update consultar.php

--- estudiante/observador/funciones/consultar.php
+++ estudiante/observador/funciones/consultar.php
@@ -3,27 +3,12 @@ include_once "../configuracion/conexion.php";
 
 // Realiza la consulta a la base de datos
 try {
- // Obtener los grados de la base de datos
- $gradosSql = "SELECT * FROM grado";
- $gradosStmt = $base_de_datos->prepare($gradosSql);
- $gradosStmt->execute();
- $grados = $gradosStmt->fetchAll(PDO::FETCH_ASSOC);
- 
- // Verifica si la consulta devuelve resultados
- if ($grados === false || count($grados) == 0) {
-     echo "No se encontraron grados en la base de datos.";
- } else {
-     echo "Se encontraron " . count($grados) . " grados.";
- }
- 
- 
- // Obtener los cursos de la base de datos
- $cursosSql = "SELECT *  FROM cursos";
- $cursosStmt = $base_de_datos->prepare($cursosSql);
- $cursosStmt->execute();
- $cursos = $cursosStmt->fetchAll(PDO::FETCH_ASSOC);
-    
+
+    $grados = $base_de_datos->query("SELECT * FROM grado")->fetchAll(PDO::FETCH_ASSOC);
+    $cursos = $base_de_datos->query("SELECT * FROM cursos")->fetchAll(PDO::FETCH_ASSOC);
     $jornadas = $base_de_datos->query("SELECT * FROM jornada")->fetchAll(PDO::FETCH_ASSOC);
+    // Verificar que los grados se han obtenido correctamente
+
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
     exit();

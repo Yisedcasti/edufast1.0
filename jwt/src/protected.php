91707c5e97db8c9035b3558a536bccf26429da1a
Yisedcasti <yisedcastiblanco@gmail.com>
2025-02-23 21:50:35 -0500

    new

--- jwt/src/protected.php
+++ jwt/src/protected.php
@@ -13,6 +13,16 @@ $authHeader = $headers['Authorization'] ?? '';
 if(!$authHeader || !preg_match('/Bearner\s(\s+)/', $authHeader, $matches))
 {
     echo json_encode(['error' => 'Token no proporcionado']);
+    exit;
+}
+$jwt = $matches[1];
 
+try{
+    $decode = JWT::decode($jwt, new Key($SECRET_KEY, 'HS256'));
+    echo json_encode(["message " => "Accesos concedido", "user" => $decode]);
+
+}
+catch(Exception $e){
+    echo json_encode(['error' => 'Token invalido']);
 }
 ?>

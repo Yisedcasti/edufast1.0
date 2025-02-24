933a10ef5f5c91a8b99d6a214278eb569f0da906
Yisedcasti <yisedcastiblanco@gmail.com>
2025-02-23 21:00:52 -0500

    new

--- jwt/public/login.php
+++ jwt/public/login.php
@@ -1,3 +1,21 @@
 <?php
- 
+ require_once '../config/database.php';
+ require_once '../vendor/autoload.php';
+
+use Firebase\JWT\JWT;
+
+$config = require '../config/clave.php';
+$SECRET_KEY = $config['SECRET_KEY'];
+
+if($_SERVER['REQUEST_METHOD'] === "POST"){
+    $num_doc = $_POST['num_doc'];
+    $contraseña = $_POST['contraseña'];
+
+    if(!$num_doc || !$contraseña){
+        echo json_encode(array("error" => "Faltan Datos"));
+        exit;
+    }
+    
+}
+
 ?>

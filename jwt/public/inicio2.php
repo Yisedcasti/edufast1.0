95cf8ce0dbf6e86ea5ad197048980e148a97c014
Yisedcasti <yisedcastiblanco@gmail.com>
2025-02-25 21:29:50 -0500

    update

--- jwt/public/inicio2.php
+++ jwt/public/inicio2.php
@@ -1,4 +1,10 @@
-
+<?php
+session_start();
+if(!isset($_SESSION['user'])){
+    header('Location: ../src/protected.php');
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -7,7 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/inicio2.css">
+    <link rel="stylesheet" href="../../css/inicio2.css">
     <title>Login</title>
 </head>
 
@@ -15,7 +21,7 @@
     <header class="navbar navbar-expand-lg bg-body-tertiary containernav shadow">
         <div class="container d-flex justify-content-between align-items-center">
             <a class="navbar-brand fw-bold text-success d-flex align-items-center gap-2" href="#">
-                <img src=" imagenes/logo.png" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">
+                <img src="../../imagenes/logo.png" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">
                 <span class="text-white">EDUFAST</span>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
@@ -40,7 +46,7 @@
 
     <main class="main-container">
         <div class="container mt-5 d-flex justify-content-center p-3">
-            <form method="POST" class="col-md-6">
+            <form action="login.php" method="POST" class="col-md-6">
                 <h1 class="text-center">Inicio de sesión</h1>
 
                 <?php if (isset($error_message)): ?>
@@ -51,13 +57,13 @@
 
                 <div class="mb-3">
                     <label for="user" class="form-label ps-3"><b>Usuario:</b></label>
-                    <input type="text" class="form-control" name="user" placeholder="Ingresa tu usuario" required>
+                    <input type="text" class="form-control" name="num_doc" placeholder="Ingresa tu numero de documento" required>
                     <small class="form-text">Asegúrate de introducir el usuario correcto.</small>
                 </div>
 
                 <div class="mb-3">
                     <label for="pass" class="form-label ps-3"><b>Contraseña:</b></label>
-                    <input type="password" class="form-control" name="pass" placeholder="Ingresa tu contraseña" required>
+                    <input type="password" class="form-control" name="contraseña" placeholder="Ingresa tu contraseña" required>
                     <small class="form-text">Asegúrate de introducir la contraseña correcta.</small>
                 </div>
 

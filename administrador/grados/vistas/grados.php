21e76ec8120057c8596cdff2c059cd52fc8e9dbf
Yisedcasti <yisedcastiblanco@gmail.com>
2025-03-14 00:07:50 -0500

    Update grados.php

--- administrador/grados/vistas/grados.php
+++ administrador/grados/vistas/grados.php
@@ -31,7 +31,6 @@ include_once "consulta.php";
                 <a href="../../materiaphp/materia.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">Materias</a>
                 <a href="../../logrophp/logros.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">Logros</a>
                 <a href="../../actividad/actividad.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">Actividades</a>
-                <a href="../../Observador/view/vista_o.html" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">Observador</a>
                 <a href="../../pag_principal.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">Volver</a>
             </div>
         </div>

b3d7177e8cb2fd6787eb1bc08bf75a07a67ad2ec
Yisedcasti <yisedcastiblanco@gmail.com>
2025-03-26 23:40:00 -0500

    Update grados.php

--- cordinador/grados/vistas/grados.php
+++ cordinador/grados/vistas/grados.php
@@ -17,7 +17,7 @@ include_once "consulta.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="../../../css/grados.css"/>
-    <link rel="stylesheet" href="../../../css/stylsadm.css"/>
+    <link rel="stylesheet" href="../../../css/stylscoor.css"/>
     <title>Grados</title>
 </head>
 
@@ -80,7 +80,7 @@ include_once "consulta.php";
 ?>
                 <main class="main-container">
         <section class="container">
-            <h2>Grados Existentes</h2>
+            <h2 class=" text-white mb-4">Grados Existentes</h2>
             
             <table class="table Regular shadow">
                 <thead>

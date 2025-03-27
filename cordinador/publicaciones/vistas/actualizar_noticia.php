5e08734d25098cd6ad308e2f50d24afdfa7f1534
Yisedcasti <yisedcastiblanco@gmail.com>
2025-03-26 23:30:00 -0500

    Update actualizar_noticia.php

--- cordinador/publicaciones/vistas/actualizar_noticia.php
+++ cordinador/publicaciones/vistas/actualizar_noticia.php
@@ -1,12 +1,12 @@
 <?php
-session_start();
 include_once "../funciones/consulta.php";
+session_start();
+if (!isset($_SESSION['user'])) {
+    $_SESSION['error_message'] = "Debes iniciar sesión para acceder a esta página.";
+    header('Location: ../src/protected.php');
+    exit;
+}
 
-// Verificar si la sesión está activa y si el usuario está autenticado
-if (!isset($_SESSION['userId'])) {
-    header("Location: ../session.php");
-    exit();
-} 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,7 +17,7 @@ if (!isset($_SESSION['userId'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-    <link rel="stylesheet" href="../../../css/nav.css"/>
+    <link rel="stylesheet" href="../../../css/stylscoor.css"/>
   
     <title>Pagina Principal</title>
 </head>
@@ -57,15 +57,15 @@ if (!isset($_SESSION['userId'])) {
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="actualizar_evento.php">Eventos</a>
+    <a class="nav-link text-white active" aria-current="page" href="actualizar_evento.php">Eventos</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="publicaciones_crear.php">Volver</a>
+    <a class="nav-link text-white active" aria-current="page" href="publicaciones_crear.php">Volver</a>
   </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle second-text fw-bold" href="#" id="navbarDropdown"
+                            <a class="nav-link dropdown-toggle text-white fw-bold" href="#" id="navbarDropdown"
                                 role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="fas fa-user me-2"></i><?php echo $_SESSION['user']; ?> <?php echo $_SESSION['usera']; ?>
+                                <i class="fas fa-user me-2"></i><?php echo $_SESSION['nombres']; ?> <?php echo $_SESSION['apellidos']; ?>
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="../../../admin/cerrar.php">Salir</a></li>
@@ -94,7 +94,7 @@ if (!isset($_SESSION['userId'])) {
                           }
                         ?>
 <section class="main-container">
-        <h1 class="titulo mb-4 text-center"> Actualizar Noticias</h1>
+        <h1 class="titulo mb-4 text-center text-white"> Actualizar Noticias</h1>
 
     <table class="table table-bordered text-center align-middle">
     <thead class="table-secondary">

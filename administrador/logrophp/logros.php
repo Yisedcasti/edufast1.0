772b871194b2366608c296e07c649829cdd09740
Yisedcasti <yisedcastiblanco@gmail.com>
2025-03-29 00:21:11 -0500

    Update logros.php

--- administrador/logrophp/logros.php
+++ administrador/logrophp/logros.php
@@ -98,8 +98,8 @@ include "consultarLogro.php";
                                             <section class="card-body">
                                                 <div class="card-color">
                                                     <p class="card-text text-left">codigo : <?php echo htmlspecialchars($logro->id_logro); ?> <h5 class="text-center"> Nombre : <?php echo htmlspecialchars($logro->nombre_logro); ?></h5></p>
-                                                    <p class="card-text text-center"> Materia : <?php echo htmlspecialchars($logro->materia); ?></p><p class="card-text text-center"> Grado : <?php echo htmlspecialchars($logro->id_grado); ?></p>
                                                     <p class="card-text text-left"> descripción: <?php echo htmlspecialchars($logro->descripcion_logro); ?></p>
+                                                    <p class="card-text text-left"> Materia : <?php echo htmlspecialchars($logro->materia); ?></p><p class="text-left"> Grado : <?php echo htmlspecialchars($logro->grado); ?></p>
 
                                                     <div class="d-flex justify-content-between">
                                                         <button type="button" class="btn btn-outline-dark boton-carrito" data-bs-toggle="modal" data-bs-target="#eliminarModal<?=$logro->id_logro?>"><i class="fas fa-trash-alt"></i></button>

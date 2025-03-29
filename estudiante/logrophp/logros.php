f24fce9b48e78fb7c5567dba561290ba7a4c2a57
Yisedcasti <yisedcastiblanco@gmail.com>
2025-03-29 00:41:00 -0500

    Update logros.php

--- estudiante/logrophp/logros.php
+++ estudiante/logrophp/logros.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+if (!isset($_SESSION['user'])) {
+    $_SESSION['error_message'] = "Debes iniciar sesión para acceder a esta página.";
+    header('Location: ../src/protected.php');
+    exit;
+}
 include "consultarLogro.php";
 
 ?>
@@ -61,7 +67,7 @@ include "consultarLogro.php";
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle second-text fw-bold" href="#" id="navbarDropdown"
                                 role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="fas fa-user me-2"></i>Maria Camila Torres Jaramillo
+                                <i class="fas fa-user me-2"></i><?php echo $_SESSION['nombres']; ?> <?php echo $_SESSION['apellidos']; ?>
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="#">Salir</a></li>

728c6fe35b5c49b3418bcac06d2da3753ee51ca5
Yisedcasti <yisedcastiblanco@gmail.com>
2025-03-28 23:52:36 -0500

    Update actualizar.php

--- administrador/Materiaphp/actualizar.php
+++ administrador/Materiaphp/actualizar.php
@@ -3,19 +3,16 @@
 include_once "conexion.php";
 $id_materia = $_POST["id_materia"]; 
 $materia = $_POST["materia"];
-$grado_id_grado = $_POST["grado_id_grado"];
 $area_id_area = $_POST["area_id_area"];
 
 // Preparar la sentencia de actualización
 $sentencia = $base_de_datos->prepare("UPDATE materia 
                                       SET materia = ?, 
-                                          grado_id_grado = ?, 
                                           area_id_area = ?
                                       WHERE id_materia = ?");
 
 $resultado = $sentencia->execute([
     $materia, 
-    $grado_id_grado, 
     $area_id_area, 
     $id_materia
 ]);

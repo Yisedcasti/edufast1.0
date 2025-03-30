d5396588af75e9089455c800ea80a72a0848c3c9
Yisedcasti <yisedcastiblanco@gmail.com>
2025-03-29 23:49:51 -0500

    Update logros.php

--- administrador/logrophp/logros.php
+++ administrador/logrophp/logros.php
@@ -98,7 +98,7 @@ include "consultarLogro.php";
                                             <section class="card-body">
                                                 <div class="card-color">
                                                     <p class="card-text text-left">codigo : <?php echo htmlspecialchars($logro->id_logro); ?> <h5 class="text-center"> Nombre : <?php echo htmlspecialchars($logro->nombre_logro); ?></h5></p>
-                                                    <p class="card-text text-left"> descripción: <?php echo htmlspecialchars($logro->descripcion_logro); ?></p>
+                                                    <p class="card-text text-left">  <?php echo htmlspecialchars($logro->descripcion_logro); ?></p>
                                                     <p class="card-text text-left"> Materia : <?php echo htmlspecialchars($logro->materia); ?></p><p class="text-left"> Grado : <?php echo htmlspecialchars($logro->grado); ?></p>
 
                                                     <div class="d-flex justify-content-between">

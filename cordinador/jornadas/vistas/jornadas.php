d2b793590af2bf8969ad51180404229f53d5d0bc
Yisedcasti <yisedcastiblanco@gmail.com>
2025-03-30 01:08:07 -0500

    Update jornadas.php

--- cordinador/jornadas/vistas/jornadas.php
+++ cordinador/jornadas/vistas/jornadas.php
@@ -36,7 +36,7 @@ if (!isset($_SESSION['user'])) {
                 <a href="../../asistencia/listados.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">Asistencias</a>
                 <a href="../../notas/notas.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">Notas</a>
                 <a href="../../Boletin/view/boletin.html" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">Boletin</a>
-                <a href="../../pag_principal.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">Volver</a>            </div>
+                <a href="../../pag_principal.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">Principal</a>            </div>
         </div>
 
         <div id="page-content-wrapper">

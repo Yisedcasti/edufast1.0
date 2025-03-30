91284239750436463fa17ccb848c4441f79af1d3
Yisedcasti <yisedcastiblanco@gmail.com>
2025-03-29 23:30:43 -0500

    Update consulta.php

--- cordinador/Materiaphp/consulta.php
+++ cordinador/Materiaphp/consulta.php
@@ -1,8 +1,7 @@
 <?php
 include "Conexion.php"; 
-$sentencia = $base_de_datos->query(" SELECT materia.*, grado.*, area.*
+$sentencia = $base_de_datos->query(" SELECT materia.*, area.*
 FROM materia
-INNER JOIN grado ON materia.grado_id_grado = grado.id_grado
 INNER JOIN area ON materia.area_id_area = area.id_area
 ORDER BY area.nombre_area ASC;  
 ;");

d6077762b7e5f1f995fbbe71e9e5e4391a7904b4
Dylan5316 <dylan.herrera.espinosa@gmail.com>
2025-05-07 21:19:10 -0500

    cursos

--- administrador/Cursos/vistas/Curso.php
+++ administrador/Cursos/vistas/Curso.php
@@ -20,7 +20,19 @@ if (!isset($_SESSION['user'])) {
     <link rel="stylesheet" href="../../../css/stylsadm.css"/>
     <title>Pagina Principal</title>
 </head>
-
+<style>
+.shadow{
+        box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px !important;
+            }
+    .alert-success{
+        background-color:#dcfce7;
+        color:#016630;
+    }
+    .alert-danger{
+        background-color:#ffc9c9;
+        color:#9f0712;
+    }
+</style>
 <body>
     <div class="d-flex" id="wrapper">
     <div class="listado" id="sidebar-wrapper">
@@ -68,8 +80,11 @@ if (!isset($_SESSION['user'])) {
             <?php
  if (isset($_GET['status'])) {
   if ($_GET['status'] == 'success') {
-      echo '<div class="alert alert-success alert-dismissible fade show" role="alert" id="autoCloseAlert">
-              ¡Accion realizada exitosamente!
+      echo '<div class="alert alert-success border-0 alert-dismissible fade show" role="alert" id="autoCloseAlert">
+             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="20" height="20" class="me-2">
+  <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+</svg>
+ ¡Accion realizada exitosamente!
               <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>';
   } elseif ($_GET['status'] == 'error') {

af201cd79ed2f5d4dfa263ca1a3ad20867ed098b
Dylan5316 <dylan.herrera.espinosa@gmail.com>
2025-05-07 20:21:44 -0500

    grados

--- administrador/grados/vistas/grados.php
+++ administrador/grados/vistas/grados.php
@@ -82,8 +82,8 @@ $grados = $gradoModelo->obtenerTodos();
 <?php
  if (isset($_GET['status'])) {
   if ($_GET['status'] == 'success') {
-      echo '<div class="alert alert-success alert-dismissible fade show" role="alert" id="autoCloseAlert">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="20" height="20" class="me-2">
+      echo '<div class="alert fs-4 alert-success alert-dismissible fade show" role="alert" id="autoCloseAlert">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="32" height="32" class="me-2">
   <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
 </svg>
   ¡Accion realizada exitosamente!
